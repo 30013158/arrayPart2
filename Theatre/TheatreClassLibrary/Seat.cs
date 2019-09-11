a4aeb0a3206011249de5c60c08ef9230eadaaa06
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-09-12 10:40:16 +1200

    first

---
+++ Theatre/TheatreClassLibrary/Seat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatreClassLibrary
+{
+  public  class Seat
+    {
+        //Based on required properties
+        public bool Booked { get; set; } = false;
+        public string AdultorChild { get; set;}
+
+        //constructor
+        public Seat(bool booked, string adultOrChild)
+        {
+            Booked = booked;
+            AdultorChild = adultOrChild;
+        }
+    }
+}
---
+++ Theatre/TheatreClassLibrary/Theater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatreClassLibrary
+{
+   public class Theater
+    {
+        public static Seat[,] theater1 = new Seat[5, 5];
+        public static Seat[,] theater2 = new Seat[5, 5];
+        public static Seat[,] theater3 = new Seat[5, 5];
+    }
+}
